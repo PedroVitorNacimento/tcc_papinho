b1d7c9661ec44456d1c67dc851acec40f931f994
PedroVitorNacimento <p.nac124111@gmail.com>
2025-04-06 22:18:13 -0300

    criando pasta imagens, inserindo barra de boas vindas na front_home

--- frontend/frontend_home.php
+++ frontend/frontend_home.php
@@ -17,15 +17,40 @@ if (empty($_SESSION['id_responsavel'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="/assets/css/style.css" />
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        rel="stylesheet" />
+
+    <title>Papinho-Home</title>
 </head>
 
-<body>
-    <h1>LOGADO</h1> <?= $_SESSION["id_responsavel"] ?>
+<body id="home-body">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
 
-    <br>
-    <!-- redireciona para o php que executa o logout -->
-    <a href="http://localhost/TCC_PAPINHO/backend/backand_logout.php">Sair</a>
+            <a class="navbar-brand" href="#">
+                <img src="/assets/imagens/Papinho_logo.png" class="rounded-circle me-2" width="40" height="40">
+                Bem vindo(a): <?php echo $_SESSION['nome_responsavel']; ?>!</a>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto"> <!-- ms-auto alinha à direita -->
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="frontend_home.php">Início</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Gerar relatorio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="http://localhost/TCC_PAPINHO/backend/backand_logout.php">Sair</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
 </body>
 
 </html>
