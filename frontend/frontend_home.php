9e61acf2c2bb8fd489a0f7f8105e417802939007
PedroVitorNacimento <p.nac124111@gmail.com>
2025-04-14 22:29:23 -0300

    inserindo a div com as imagens a serem clicadas

--- frontend/frontend_home.php
+++ frontend/frontend_home.php
@@ -18,8 +18,7 @@ if (empty($_SESSION['id_responsavel'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/assets/css/style.css" />
-    <!-- CSS externo personalizado -->
-    <link rel="stylesheet" href="/assets/css/categorias.css">
+
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         rel="stylesheet" />
@@ -52,17 +51,20 @@ if (empty($_SESSION['id_responsavel'])) {
             </div>
         </div>
     </nav>
-    <!-- titulo da tela -->
-    <div class="titulo">Escolha uma categoria</div>
+    <div class="main-content">
+        <!-- titulo da tela -->
+        <div class="titulo">Escolha uma categoria</div>
 
-    <!--grid com imagens representando categorias-->
-    <div class="grid-categorias">
-        <img src="/assets/imagens/comer.png" class="categoria" onclick="tocarSom('comer')">
-        <img src="/assets/imagens/brincar.png" class="categoria" onclick="tocarSom('brincar')">
-        <img src="/assets/imagens/banheiro.png" class="categoria" onclick="tocarSom('banheiro')">
-        <img src="/assets/imagens/dormir.png" class="categoria" onclick="tocarSom('dormir')">
-        <img src="/assets/imagens/triste.png" class="categoria" onclick="tocarSom('triste')">
-        <img src="/assets/imagens/feliz.png" class="categoria" onclick="tocarSom('feliz')">
+        <!--grid com imagens representando categorias-->
+        <div class="grid-categorias">
+            <img src="/assets/imagens/comer.png" class="categoria" onclick="tocarSom('comer')">
+            <img src="/assets/imagens/brincar.png" class="categoria" onclick="tocarSom('brincar')">
+            <img src="/assets/imagens/banheiro.png" class="categoria" onclick="tocarSom('banheiro')">
+            <img src="/assets/imagens/dormir.png" class="categoria" onclick="tocarSom('dormir')">
+            <img src="/assets/imagens/triste.png" class="categoria" onclick="tocarSom('triste')">
+            <img src="/assets/imagens/feliz.png" class="categoria" onclick="tocarSom('feliz')">
+        </div>
+
     </div>
 </body>
 
