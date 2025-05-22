37d7ad0bd7568dea2187c655db093e3c17f0dde1
PedroVitorNacimento <p.nac124111@gmail.com>
2025-05-22 15:26:26 -0300

    ajuste de titulo do formulario e icones do botão na pagina cadastro

--- frontend/frontend_cadastro.php
+++ frontend/frontend_cadastro.php
@@ -30,7 +30,7 @@
   <div class="container mt-4">
     <div class="form-container1">
       <!-- Formulário de cadastro -->
-
+      <h4 class="mb-4 text-center">Cadastro de Usuario</h4>
       <!-- Campos do formulário -->
       <label>Nome:</label>
       <div class="input-group mb-3">
@@ -68,8 +68,17 @@
 
       <!-- Botões -->
       <div class="d-flex justify-content-between">
-        <button class="btn btn-secondary" onclick="history.back()">Voltar</button>
-        <button class="btn btn-primary" onclick="salvaFormulario()">Salvar</button>
+        <button class="btn btn-primary" onclick="history.back()">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z" />
+            <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z" />
+          </svg>
+          Voltar</button>
+        <button class="btn btn-primary" onclick="salvaFormulario()">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save2" viewBox="0 0 16 16">
+            <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v4.5h2a.5.5 0 0 1 .354.854l-2.5 2.5a.5.5 0 0 1-.708 0l-2.5-2.5A.5.5 0 0 1 5.5 6.5h2V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1z" />
+          </svg>
+          Salvar</button>
       </div>
     </div>
   </div>
